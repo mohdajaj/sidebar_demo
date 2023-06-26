4693303c916a192412a1075cf9edaa88d8d33acb
Ajaj <mohd.ajaj@chipsoft@gmail.com>
2023-06-26 17:58:00 +0530

    sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SideBarComponent } from './components/sideBar/sideBar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'header',component:HeaderComponent},
+{path:'sideBar',component:SideBarComponent},
+{path:'footer',component:FooterComponent},
+{path:'about',component:AboutComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/sideBar/sideBar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+     AppComponent,
+     SideBarComponent,
+     HeaderComponent,
+     FooterComponent,
+     AboutComponent
+    
+
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
